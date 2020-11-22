dd26aad6d23a7e833887700514b285d72c767051
xlyver <62627615+xlyver@users.noreply.github.com>
2020-11-22 00:08:07 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olyver_DecimalBinario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            string bin = "";
+            Console.Write("Introduce el número decimal:");
+            n = int.Parse(Console.ReadLine());
+
+            while (true)
+            {
+                if ((n % 2) != 0)
+                    bin = "1" + bin;
+                else
+                    bin = "0" + bin;
+                n /= 2;
+                if (n <= 0)
+                    break;
+            }
+            Console.WriteLine("El resultado en binario:" + bin);
+            Console.ReadKey();
+        }
+    }
+}
